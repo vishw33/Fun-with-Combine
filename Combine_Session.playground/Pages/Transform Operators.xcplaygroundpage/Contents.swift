e4fd754d1b9561bdc0a4742dd669a4add2668e2b
Vishwas <vishw33@yahoo.com>
2020-02-10 11:15:15 +0530

    Filtering Operators Resource

--- Combine_Session.playground/Pages/Transform Operators.xcplaygroundpage/Contents.swift
+++ Combine_Session.playground/Pages/Transform Operators.xcplaygroundpage/Contents.swift
@@ -25,7 +25,6 @@ _ = publisher.sink(receiveValue: { (val) in
 })
 
 //: using Operator
-
 _ = publisher
     .collect()
     .sink(receiveValue: { (val) in
@@ -47,7 +46,6 @@ _ = publisher.map{
 })
 //: ## tryMap(_:)
 //: Try Map is same as map but it gives room for error handling and send another publisher in case of error
-
 //: ## Just error Example
 Just("Directory name that does not exist")
     .tryMap { try FileManager.default.contentsOfDirectory(atPath: $0) }
@@ -57,6 +55,7 @@ Just("Directory name that does not exist")
 
 //: ![](flatmap.png)
 //: ## flatMap(maxPublishers:_:)
+
 struct User {
    let name: CurrentValueSubject<String, Never>
 }
@@ -69,5 +68,42 @@ userSubject
 
 let user = User(name: .init("Me"))
 userSubject.send(user)
+
+//:  # Replacing Up Stream
+//: ![](ReplaceNil.png)
+//: ## replaceNil(with:)
+//: replace the upstream value with given value
+
+let nilArray = [1,nil,2,nil].publisher
+_ = nilArray.replaceNil(with: -1).sink(receiveValue: { (val) in
+    print(val)
+})
+//: one place Map can be explained better
+_ = nilArray.replaceNil(with: -1).map{
+    Int($0)
+}.sink(receiveValue: { (val) in
+    print(val)
+})
+
+//: ![](ReplaceEmpty.png)
+//: ## replaceEmpty(with:)
+//: replace the upstream value with given value
+let empty = Empty<Int, Never>()
+  
+  // 2
+  empty
+.replaceEmpty(with: 10)
+    .sink(receiveCompletion: { print($0) },
+          receiveValue: { print($0) })
+    .store(in: &subscriptions)
+
+//: ![](Scan.png)
+//: ## scan(_:_:)
+//: It will provide the current value emitted by an upstream publisher to a closure, along with the last value returned by that closure
+_ = publisher.scan(0){
+    $0 + $1
+}.sink(receiveValue: { (val) in
+    print(val)
+})
 
 //: [Filtering Operator](@next)
--- Combine_Session.playground/Pages/Transform Operators.xcplaygroundpage/Sources/Source.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import Combine
-
-struct User {
-   let name: CurrentValueSubject<String, Never>
-}
-
-
